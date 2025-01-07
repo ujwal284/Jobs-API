fc608942a87da67866103ac813edf9eae3c2516c
ujwal284 <ujwalkhanal284@gmail.com>
2025-01-07 09:35:56 +0545

    implement authentication and job management routes

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+// connect DB
+
+
+// routers
+const authRouter = require('./routes/auth.js')
+const jobsRouter = require('./routes/jobs.js')
+
+
+
+
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -11,9 +22,8 @@ app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-    res.send('jobs api');
-});
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', jobsRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
