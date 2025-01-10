56b1eabf439bc086e0be6f7dbe9a31790f36dbdc
ujwal284 <ujwalkhanal284@gmail.com>
2025-01-10 10:54:37 +0545

    feat: implement user authentication with JWT and password hashing

---
+++ middleware/authentication.js
@@ -0,0 +1,26 @@
+const User = require('../models/User.js')
+const jwt = require('jsonwebtoken')
+const { UnauthenticatedError } = require('../errors')
+
+
+const auth = (req, res, next) => {
+    // check header
+    const authHeader = req.headers.authorization
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
+        throw new UnauthenticatedError('Authentication invalid')
+    }
+
+    const token = authHeader.split(' ')[1]
+
+    try {
+        const playload = jwt.verify(token, process.env.JWT_SECRET)
+        // attach the user to the job routes
+        req.user = { userId: playload.userId, name: playload.name }
+        next()
+    } catch (error) {
+        throw new UnauthenticatedError('Authentication invalid')
+    }
+}
+
+
+module.exports = auth
