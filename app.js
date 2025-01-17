948cf04ff95f4f4298ef6f99c614146b6ed94448
ujwal284 <ujwalkhanal284@gmail.com>
2025-01-17 10:24:23 +0545

    feat: enhance security with helmet, cors, xss-clean, and rate limiting; improve error handling for validation errors

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+
+
 const express = require('express');
 const app = express();
 
@@ -19,8 +27,18 @@ const authenticateUser = require('./middleware/authentication.js')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+
+app.set('trust proxy', 1)
+app.use(
+    rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // limit each IP to 100 requests per windowMs
+    })
+)
 app.use(express.json());
-// extra packages
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes
 app.use('/api/v1/auth', authRouter)
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -10,14 +10,23 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     // if (err instanceof CustomAPIError) {
     //     return res.status(err.statusCode).json({ msg: err.message })
     // }
+
+    if (err.name === 'ValidationError') {
+        console.log(Object.values(err.errors));
+
+        customError.msg = Object.values(err.errors).map((item) => item.message).join(',')
+        customError.statusCode = 400
+    }
+
+
     if (err.code && err.code === 11000) {
         customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`
         customError.statusCode = 400
     }
 
 
-    // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-    return res.status(customError.statusCode).json({ msg: customError.msg })
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+    // return res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
 module.exports = errorHandlerMiddleware
